a5d00a858cdabbbda6e7592a516941ea351997cc
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-03-22 08:19:28 -0400

    Updated from Brython Server: 3/22/2018 8:19:27 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -9,13 +9,20 @@ ROWS = 20
 COLS = 40
 CELL_SIZE = 20
 
+
+def moveRight(event):
+    monkey.x += CELL_SIZE
 if __name__ == '__main__':
     
     #colors
     green = Color(0x006600,1)
+    brown = Color(0x884513,1)
     
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
+    monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
     
     Sprite(jungleBox)
+    monkey = Sprite(monkeyBox)
     
+    App().listenKeyEvent('keydown','right arrow',moveRight)
     App().run()

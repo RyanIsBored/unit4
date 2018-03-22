d082e3618b3aa4d104243aba2b758b7b2a758d04
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-03-22 08:08:48 -0400

    Updated from Brython Server: 3/22/2018 8:08:47 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -1,1 +1,16 @@
+#Ryan Jones
+#3/22/18
+#Monkey Quest 2.0
 
+from ggame import *
+
+if __name__ == '__main__':
+    
+    #colors
+    green = Color(0x006600,1)
+    
+    jungleBox = RectangleAsset(800,500,LineStyle(1,green),green)
+    
+    Sprite(jungleBox)
+    
+    App().run()

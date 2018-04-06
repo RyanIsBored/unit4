0e377a60a1e7ff5b0f241d0809dc61a97cf7cd77
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-04-06 08:35:36 -0400

    Updated from Brython Server: 4/6/2018 8:35:35 AM

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,24 @@
 #Ryan Jones
 #4/4/18
 
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+
 def printBoard():
-    print('|           |')
-    print('|           |
+    print('        |     |     ')
+    print('        |     |     ')
+    print('   -----------------')
+    print('        |     |     ')
+    print('        |     |     ')
+    print('   -----------------')
+    print('        |     |     ')
+    print('        |     |     ')
+
+printBoard()

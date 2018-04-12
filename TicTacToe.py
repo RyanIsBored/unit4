fec142e72186705e3dfff6d2f28361f24c4dd50a
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-04-12 15:07:01 -0400

    Updated from Brython Server: 4/12/2018 3:07:00 PM

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,6 +13,28 @@ g=7
 h=8
 i=9
 
+def isEmpty(square):
+    if square==1 and a==1:
+        return True
+    if square==2 and b==2:
+        return True
+    if square==3 and c==3:
+        return True
+    if square==4 and d==4:
+        return True
+    if square==5 and e==5:
+        return True
+    if square==6 and f==6:
+        return True
+    if square==7 and g==7:
+        return True
+    if square==8 and h==8:
+        return True
+    if square==9 and i==9:
+        return True
+    return False
+        
+        
 def printBoard():
     print('      |   |     ')
     print('  ',a,' |',b,'|',c,'')
@@ -46,4 +68,24 @@ if __name__ == '__main__':
         if choice==9:
             i='x'
         printBoard()
-    
+        choice2 = randint(1,9)
+        if choice2==1:
+            a='o'
+        if choice2==2:
+            b='o'
+        if choice2==3:
+            c='o'
+        if choice2==4:
+            d='o'
+        if choice2==5:
+            e='o'
+        if choice2==6:
+            f='o'
+        if choice2==7:
+            g='o'
+        if choice2==8:
+            h='o'
+        if choice2==9:
+            i='o'
+        printBoard()
+        
